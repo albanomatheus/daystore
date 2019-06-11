e2e42e19c280300b509691ffd05b127ae5ba6c38
Bittencourt <davyblima@live.com>
2019-06-11 20:54:57 -0300

    rodape

--- pages/footer.php
+++ pages/footer.php
@@ -1,3 +1,46 @@
 <div class="footer navbar-fixed-bottom">
-	Footer
+    <div class="row">
+        <div class="form-group col-md-2">
+            <div style=" font-size: 0.75em; border-left: 2px purple solid; width: auto%; padding-left: 10px;">
+                <div style="right: 2px; text-decoration: underline purple;  color: purple;">Contato</div>
+                <div style="right: 2px; color: purple;">E-mail: daystore@gmail.com</div>
+                <div style="right: 2px; color: purple;">Telefone: (34)99999-9999</div>
+            </div>
+        </div>
+        <div class="form-group col-md-2">
+            <div style=" font-size: 0.75em; border-left: 2px purple solid; width: auto%; padding-left: 10px;">
+                <div style="right: 2px; text-decoration: underline purple;  color: purple;">Contato</div>
+                <div style="right: 2px; color: purple;">E-mail: daystore@gmail.com</div>
+                <div style="right: 2px; color: purple;">Telefone: (34)99999-9999</div>
+            </div>
+        </div>
+        <div class="form-group col-md-2">
+            <div style=" font-size: 0.75em; border-left: 2px purple solid; width: auto%; padding-left: 10px;">
+                <div style="right: 2px; text-decoration: underline purple;  color: purple;">Contato</div>
+                <div style="right: 2px; color: purple;">E-mail: daystore@gmail.com</div>
+                <div style="right: 2px; color: purple;">Telefone: (34)99999-9999</div>
+            </div>
+        </div>
+        <div class="form-group col-md-2">
+            <div style=" font-size: 0.75em; border-left: 2px purple solid; width: auto%; padding-left: 10px;">
+                <div style="right: 2px; text-decoration: underline purple;  color: purple;">Contato</div>
+                <div style="right: 2px; color: purple;">E-mail: daystore@gmail.com</div>
+                <div style="right: 2px; color: purple;">Telefone: (34)99999-9999</div>
+            </div>
+        </div>
+        <div class="form-group col-md-2">
+            <div style=" font-size: 0.75em; border-left: 2px purple solid; width: auto%; padding-left: 10px;">
+                <div style="right: 2px; text-decoration: underline purple;  color: purple;">Contato</div>
+                <div style="right: 2px; color: purple;">E-mail: daystore@gmail.com</div>
+                <div style="right: 2px; color: purple;">Telefone: (34)99999-9999</div>
+            </div>
+        </div>
+        <div class="form-group col-md-2">
+            <div style=" font-size: 0.75em; border-left: 2px purple solid; width: auto%; padding-left: 10px;">
+                <div style="right: 2px; text-decoration: underline purple;  color: purple;">Contato</div>
+                <div style="right: 2px; color: purple;">E-mail: daystore@gmail.com</div>
+                <div style="right: 2px; color: purple;">Telefone: (34)99999-9999</div>
+            </div>
+        </div>
+    </div>
 </div>
