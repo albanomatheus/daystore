ded550a5f8dd00d74c6d697575903029559c955d
Bittencourt <davyblima@live.com>
2019-06-11 21:23:12 -0300

    fixing 11/06 -> 21:23

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ include "_back/db/connection.php";
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/lib/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script type="text/javascript" src="../daystore/assets/js/scripts.js"></script>
+    <script type="text/javascript" src="../daystore/assets/js/jquery-3.3.1.min.js"></script>
 </head>
 <body>
 <div class="main">
