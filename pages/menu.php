7b78687db055af591592ed86f6c09756e0de7757
matheus <albanomatheus16@gmail.com>
2019-06-12 22:11:19 -0300

    atualizacao css

--- pages/menu.php
+++ pages/menu.php
@@ -14,71 +14,38 @@ if (isset($_SESSION['user']) && isset($_GET['num'])) {
 <header>
     <table style="width: 100%; table-layout: fixed; height: 120px;" border="0">
         <tr>
-            <td><a href="<?php echo "?page=home&num={$key}"; ?>"><img src="assets/img/logo.png" width="100"
-                                                                      height="100"></a></td>
+            <td><a href="<?php echo "?page=home&num={$key}"; ?>"><img src="assets/img/logo.png" width="100" height="100"></a></td>
             <td align="center">
                 <a href="<?php echo "?page=cart&num={$key}"; ?>"><img src="assets/img/cart.png" width="50" height="50"></a><br><br>
                 <?php if (!empty($msg)): ?>
                     <span><?php echo $msg; ?></span>
                 <?php else: ?>
-                    <div class="row">
-                        <div class="form-group col-md-4">
-                            <a href="<?php echo "?page=admin&num={$key}"; ?>">
-                                <button class="searchButton">Entrar como Adm</button>
-                            </a>
-                        </div>
-                        <div class="form-group col-md-4">
-                            <a href="<?php echo "?page=login&num={$key}"; ?>">
-                                <button class="searchButton">Entrar</button>
-                            </a>
-                        </div>
-                        <div class="form-group col-md-4">
-                            <a href="<?php echo "?page=sign&num={$key}"; ?>">
-                                <button class="searchButton">Criar uma Conta</button>
-                            </a>
-                        </div>
-                    </div>
+                    <a href="<?php echo "?page=admin&num={$key}"; ?>"><button class="searchButton">Entrar como Adm</button></a>
+                    <a href="<?php echo "?page=login&num={$key}"; ?>"><button class="searchButton">Entrar</button></a>
+                    <a href="<?php echo "?page=sign&num={$key}"; ?>"><button class="searchButton">Criar uma Conta</button></a>
                 <?php endif; ?>
             </td>
             <td align="right">
                 <form method="GET">
                     <input type="hidden" name="page" value="find">
                     <input type="hidden" name="num" value="<?php echo $key; ?>">
-                    <input type="text" id="searchText" name="tag" placeholder="Buscar..."/>
+                    <input type="texto" id="searchText" name="tag" placeholder="Buscar..."/>
                     <button class="searchButton" style="width: 211px;">Buscar</button>
                 </form>
             </td>
         </tr>
     </table>
 </header>
-<div id="menu">
+<div id = "menu">
     <ul>
-        <a href="<?php echo "?page=home&num={$key}"; ?>">
-            <li>Todos</li>
-        </a>
-        <a href="<?php echo "?page=developers&num={$key}"; ?>">
-            <li>Desenvolvimento</li>
-        </a>
-        <a href="<?php echo "?page=musica&num={$key}"; ?>">
-            <li>Música</li>
-        </a>
-        <a href="<?php echo "?page=emojis&num={$key}"; ?>">
-            <li>Emojis</li>
-        </a>
-        <a href="<?php echo "?page=games&num={$key}"; ?>">
-            <li>Jogos</li>
-        </a>
-        <a href="<?php echo "?page=beer&num={$key}"; ?>">
-            <li>Cerveja</li>
-        </a>
+        <a href="<?php echo "?page=home&num={$key}"; ?>"><li>Todos</li></a>
+        <a href="<?php echo "?page=developers&num={$key}"; ?>"><li>Desenvolvimento</li></a>
+        <a href="<?php echo "?page=musica&num={$key}"; ?>"><li>Música</li></a>
+        <a href="<?php echo "?page=emojis&num={$key}"; ?>"><li>Emojis</li></a>
+        <a href="<?php echo "?page=games&num={$key}"; ?>"><li>Jogos</li></a>
+        <a href="<?php echo "?page=beer&num={$key}"; ?>"><li>Cerveja</li></a>
         <?php if (!empty($msg)): ?>
-            <a onclick="logout()">
-                <li>Sair</li>
-            </a>
+            <a onclick="logout()"><li>Sair</li></a>
         <?php endif ?>
     </ul>
-</div>
-
-<div id="rodape">
-    <?php include 'footer.php' ?>
 </div>
